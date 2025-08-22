615671c89ad39203df40e5a0a1f7ed9616560129
Md Mihran Sohail <sohail.mihran@gmail.com>
2025-08-22 18:05:07 +0530

    Time: 3 ms (23.84%), Space: 13.5 MB (35.03%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int mostWordsFound(vector<string>& sentences) {
+        int maxWords = 0;
+        for(string s : sentences){
+            int count = 1;
+            for(char c : s){
+                if(c == ' '){
+                    count++;
+                }
+            }
+            maxWords = max(count,maxWords);
+        }
+        return maxWords;
+    }
+};
