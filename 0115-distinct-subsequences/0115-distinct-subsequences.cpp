fc3645fdafe52033d06a64a60a22be678054154e
Md Mihran Sohail <sohail.mihran@gmail.com>
2026-09-06 15:21:58 +0530

    Time: 3 ms (99.83%), Space: 12.3 MB (81.21%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,26 +1,37 @@
+unsigned dp[1001][1001]; // dp[i][j] denotes # for t[0:j] in s[0:i]
 class Solution {
 public:
-    int numDistinct(string s, string t) {
-        int m = s.length();
-        int n = t.length();
-
-        vector<vector<unsigned long long>> dp(
-            m + 1, vector<unsigned long long>(n + 1, 0));
-
-        for (int i = 0; i <= m; i++) {
-            dp[i][n] = 1;
-        }
-
-        for (int sIndex = m - 1; sIndex >= 0; sIndex--) {
-            for (int tIndex = n - 1; tIndex >= 0; tIndex--) {
-                dp[sIndex][tIndex] = dp[sIndex + 1][tIndex];
-
-                if (s[sIndex] == t[tIndex]) {
-                    dp[sIndex][tIndex] += dp[sIndex + 1][tIndex + 1];
-                }
-            }
-        }
-
-        return dp[0][0];
+    int m, n;
+    string_view s, t;
+    unsigned f(int i, int j) {
+        if (j==0) return 1; // base case
+        else if (i==0 || j>i) return 0;
+        if (dp[i][j] != UINT_MAX) return dp[i][j];
+        
+        unsigned ans=0;
+        if (s[i-1]==t[j-1]) 
+            ans=f(i-1, j-1) + f(i-1, j);
+        else
+            ans=f(i-1, j);
+        
+        return dp[i][j]=ans;
+    }
+    
+    int numDistinct(string& s, string& t) {
+        this->s = s;
+        this->t = t;
+        m=s.size();
+        n=t.size();
+        for(int i=0; i<=m; i++)
+            memset(dp[i], -1, (n+1)*sizeof(unsigned));
+        return f(m, n);
     }
 };
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
